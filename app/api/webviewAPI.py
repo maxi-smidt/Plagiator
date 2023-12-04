2c2d5dacc2af7cdb9e934cc201419f6fa1f2afd5
maximiliansmidt <maxismidt@icloud.com>
2023-12-04 17:54:20 +0100

    ...

--- app/api/webviewAPI.py
+++ app/api/webviewAPI.py
@@ -3,13 +3,13 @@ from ..plagiatscan.scan import computeComparison
 
 
 class API:
-    def toggleFullscreen(self):
+    def toggle_fullscreen(self):
         webview.windows[0].toggle_fullscreen()
 
     def health(self):
         print("Application works!")
 
-    def computeComparison(self, fileL, fileR):
+    def compute_comparison(self, fileL, fileR):
         print("File One")
         print(fileL)
         return computeComparison(fileL, fileR)
